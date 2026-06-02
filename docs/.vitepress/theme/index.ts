2cc2848c32e242ff596495d0754df07f028fe64c
Jeff Dickey <jdx@mini-base.local>
2026-06-02 11:38:27 -0500

    docs: use consistent en.dev footer

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,12 +2,15 @@ import DefaultTheme from "vitepress/theme";
 import type { Theme } from "vitepress";
 import { h } from "vue";
 import { initBanner } from "./banner";
+import EndevFooter from "./EndevFooter.vue";
 import "./custom.css";
 
 export default {
   extends: DefaultTheme,
   Layout() {
-    return h(DefaultTheme.Layout, null);
+    return h(DefaultTheme.Layout, null, {
+      "layout-bottom": () => h(EndevFooter),
+    });
   },
   enhanceApp() {
     initBanner();
