3848ce9c7ee647b169209dd64017d67d0ad26d2d
Jeff Dickey <jdx@mini-base.local>
2026-06-02 11:29:02 -0500

    docs: avoid duplicate footer

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,15 +2,12 @@ import DefaultTheme from "vitepress/theme";
 import type { Theme } from "vitepress";
 import { h } from "vue";
 import { initBanner } from "./banner";
-import EndevFooter from "./EndevFooter.vue";
 import "./custom.css";
 
 export default {
   extends: DefaultTheme,
   Layout() {
-    return h(DefaultTheme.Layout, null, {
-      "layout-bottom": () => h(EndevFooter),
-    });
+    return h(DefaultTheme.Layout, null);
   },
   enhanceApp() {
     initBanner();
